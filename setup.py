99e94a102300b143fc2c949616007e303f12ab7f
Victor Vega <dblvs.pr@gmail.com>
2026-04-24 18:12:23 -0400

    fix setup readme path

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
-from setuptools import setup, find_packages
-
-# put the contents of your README file into the long_description
+from setuptools import setup
 from pathlib import Path
+
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.rst").read_text()
+
+long_description = (this_directory / "README.rst").read_text(encoding="utf-8")
 
 setup(
-    long_description=long_description
+    name="django-cityloc-pkg-dblvsdev",
+    version="0.0.5",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
 )
+
