89dd1794cfc5f429e584b10fd1d8ddec3bd78dc8
Victor Vega <dblvs.pr@gmail.com>
2026-04-24 18:46:26 -0400

    simplify setup to fix build

--- setup.py
+++ setup.py
@@ -1,14 +1,11 @@
 from setuptools import setup
-from pathlib import Path
-
-this_directory = Path(__file__).parent
-
-long_description = (this_directory / "README.rst").read_text(encoding="utf-8")
 
 setup(
-    name="django-cityloc-pkg-dblvsdev",
-    version="0.0.5",
-    long_description=long_description,
-    long_description_content_type="text/x-rst",
-)
 
+    name="django-cityloc-pkg-dblvsdev",
+
+    version="0.0.7",
+
+    description="City locations Django app",
+
+)
